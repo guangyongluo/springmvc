dd5ae342f6cbb47f45b7e392dd244c4aadb47d88
guangyongluo <llooww@mail.ustc.edu.cn>
2019-04-03 14:51:36 +0800

    springmvc(com.vilin.springmvc.chapter04): spring mvc basic features. @Controller @RequestMapping @Service

--- src/main/java/com/vilin/springmvc/chapter04/controller/ProductController2.java
+++ src/main/java/com/vilin/springmvc/chapter04/controller/ProductController2.java
@@ -1,4 +1,4 @@
-package com.vilin.springmvc.chapter04;
+package com.vilin.springmvc.chapter04.controller;
 
 import com.vilin.springmvc.chapter02.design01_02.Product;
 import com.vilin.springmvc.chapter02.design01_02.ProductForm;
@@ -7,6 +7,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,6 +27,7 @@ public class ProductController2 {
         return "ProductForm";
     }
 
+    @RequestMapping(value = "/save-product")
     public String saveProduct(ProductForm productForm, RedirectAttributes redirectAttributes){
         logger.info("saveProduct called");
 
@@ -36,6 +39,13 @@ public class ProductController2 {
         Product saveProduct = productService.add(product);
 
         redirectAttributes.addFlashAttribute("message", "The product was successfully added");
-        return "redirect:/product-view" + saveProduct.getId();
+        return "redirect:/view-product/" + saveProduct.getId();
+    }
+
+    @RequestMapping(value = "/view-product/{id}")
+    public String viewProduct(@PathVariable Long id, Model model){
+        Product product = productService.get(id);
+        model.addAttribute("product", product);
+        return "ProductView";
     }
 }
