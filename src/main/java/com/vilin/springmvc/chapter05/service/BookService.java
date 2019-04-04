395c75a0c0caec6948385dd6d70013bb475ff095
guangyongluo <llooww@mail.ustc.edu.cn>
2019-04-04 14:13:49 +0800

    springmvc(com.vilin.springmvc.chapter05): spring form tags. review @ModelAttribute

---
+++ src/main/java/com/vilin/springmvc/chapter05/service/BookService.java
@@ -0,0 +1,19 @@
+package com.vilin.springmvc.chapter05.service;
+
+import java.util.List;
+
+import com.vilin.springmvc.chapter05.domain.Book;
+import com.vilin.springmvc.chapter05.domain.Category;
+
+
+public interface BookService {
+    
+    List<Category> getAllCategories();
+    Category getCategory(int id);
+    List<Book> getAllBooks();
+    Book save(Book book);
+    Book update(Book book);
+    Book get(long id);
+    long getNextId();
+
+}
